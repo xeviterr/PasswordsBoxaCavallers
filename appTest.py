868e8a2e08b632fbe6a2c94da7602dc717f63fb4
Xevi <xeviterr@gmail.com>
2020-07-04 12:37:47 +0200

    Canvis

--- appTest.py
+++ appTest.py
@@ -4,4 +4,6 @@ from app import validaPassword
 passwordCorrecte, msg = validaPassword("patata")
 assert (passwordCorrecte==False)
 passwordCorrecte, msg = validaPassword("patata@N04ad34234")
+#aquest hauria de funcionar.
 assert (passwordCorrecte==True)
+
